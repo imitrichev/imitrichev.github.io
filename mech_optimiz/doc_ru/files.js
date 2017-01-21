0d63dbb3f2babc3c0a772d6b16e9ebac5d92f9d4
imitrichev <ivan.mitrichev@gmail.com>
2017-01-21 21:31:41 +0300

    [add] Documentation for v 4.4.1 released

---
+++ mech_optimiz/doc_ru/files.js
@@ -0,0 +1,13 @@
+var files =
+[
+    [ "doc_00001_index.h", "doc__00001__index_8h.html", null ],
+    [ "doc_00011_usage.h", "doc__00011__usage_8h.html", null ],
+    [ "doc_00021_tutorials.h", "doc__00021__tutorials_8h.html", null ],
+    [ "doc_00051_dbusage.h", "doc__00051__dbusage_8h.html", null ],
+    [ "doc_00061_solver_basics.h", "doc__00061__solver__basics_8h.html", null ],
+    [ "doc_00071_number_of_clusters.h", "doc__00071__number__of__clusters_8h.html", null ],
+    [ "doc_00081_pca_analysis.h", "doc__00081__pca__analysis_8h.html", null ],
+    [ "doc_00091_lateral.h", "doc__00091__lateral_8h.html", null ],
+    [ "doc_00101_sites.h", "doc__00101__sites_8h.html", null ],
+    [ "doc_00111_TC.h", "doc__00111___t_c_8h.html", null ]
+];
---
+++ mech_optimiz/doc_ru/search/all_0.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['doc_5f00001_5findex_2eh',['doc_00001_index.h',['../doc__00001__index_8h.html',1,'']]],
+  ['doc_5f00011_5fusage_2eh',['doc_00011_usage.h',['../doc__00011__usage_8h.html',1,'']]],
+  ['doc_5f00021_5ftutorials_2eh',['doc_00021_tutorials.h',['../doc__00021__tutorials_8h.html',1,'']]],
+  ['doc_5f00051_5fdbusage_2eh',['doc_00051_dbusage.h',['../doc__00051__dbusage_8h.html',1,'']]],
+  ['doc_5f00061_5fsolver_5fbasics_2eh',['doc_00061_solver_basics.h',['../doc__00061__solver__basics_8h.html',1,'']]],
+  ['doc_5f00071_5fnumber_5fof_5fclusters_2eh',['doc_00071_number_of_clusters.h',['../doc__00071__number__of__clusters_8h.html',1,'']]],
+  ['doc_5f00081_5fpca_5fanalysis_2eh',['doc_00081_pca_analysis.h',['../doc__00081__pca__analysis_8h.html',1,'']]],
+  ['doc_5f00091_5flateral_2eh',['doc_00091_lateral.h',['../doc__00091__lateral_8h.html',1,'']]],
+  ['doc_5f00101_5fsites_2eh',['doc_00101_sites.h',['../doc__00101__sites_8h.html',1,'']]],
+  ['doc_5f00111_5ftc_2eh',['doc_00111_TC.h',['../doc__00111___t_c_8h.html',1,'']]]
+];
